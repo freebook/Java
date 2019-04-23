eda0bf095a8d5517637b743e44364ccc0606a22d
Neo Chan <netkiller@msn.com>
2019-04-23 16:35:49 +0800

    jdbc

--- mac.sh
+++ mac.sh
@@ -1,3 +1,3 @@
-rm -rf ~/tmp/management/
-mkdir -p ~/tmp/management/
-xsltproc -o ~/tmp/management/ docbook-xsl/docbook.mac.xsl book.xml
+rm -rf ~/tmp/java/
+mkdir -p ~/tmp/java/
+xsltproc -o ~/tmp/java/ docbook-xsl/docbook.mac.xsl book.xml
