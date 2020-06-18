759c04b3707ee2d8b1656b1dd1574acb8d3413f2
Neo Chan <netkiller@msn.com>
2020-06-18 09:24:04 +0800

    update

--- Spring/mac.sh
+++ Spring/mac.sh
@@ -1,3 +1,4 @@
 #rm -rf ~/tmp/spring/
 mkdir -p ~/tmp/spring/
+cp ../common/docbook.css ~/tmp/spring/
 xsltproc -o ~/tmp/spring/ ../docbook-xsl/docbook.mac.xsl book.xml
--- mac.sh
+++ mac.sh
@@ -1,3 +1,8 @@
+git pull
+git submodule init
+git submodule update
+	
 rm -rf ~/tmp/java/
 mkdir -p ~/tmp/java/
+cp common/docbook.css ~/tmp/java/
 xsltproc -o ~/tmp/java/ docbook-xsl/docbook.mac.xsl book.xml
